a38f93c518813c09023409d14865967b26c034f2
Jean-Matthieu DECHRISTE <jeanmatthieu@dechriste.fr>
2020-01-27 10:51:35 +0100

    Inital commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# About
+This project is a complement of the [old fashionned warning light](https://www.iot-experiments.com/esp32-warning-light/).
+
+At first, the ESP32 (or ESP8266) was connected to an MQTT server.
+Sadly, the corporate network is quite limited (bad WiFi connection, port closed, proxy...).
+
+To make my project work, I use a small OrangePi Zero, configured to use the corporate proxy, and connected through an Ethernet port.
+The ESP8266 is reprogrammed to read the serial port and respond to some user defined AT commands.
+The ESP8266 is connected to the OrangePI UART1 port.
+My electronic circuit is powering the ESP8266 (Wimos D1 mini at 5V) and the OrangePi, sharing a common GND.
+
+# Orange PI Zero configuration
+The Orange PI uses `Armbian Bionic with Linux 5.4.8-sunxi`.
+Note: As I wasn't sure that the power of the circuit will e stale enough for the OrangePI Zero, I limited the CPU power at 4801Mhz, thanks to `armbian-config`.
+
+I wanted to use the UART1 of the Orange PI Zero, located on pins PG06 / PG07.
+I did it the hard way (see below). Easiest solution: use the command `armbian-config` and use `System > Toggle hardware configuration`.
+
+To enable the serial port on armbian, I read the folowing file : `/boot/dtb/overlay/README.sun8i-h3-overlays`
+Then, I edited `/boot/armbianEnv.txt` to add `uart1` to the `overlays` parameter.
+After a reboot, the serial port `/dev/ttyS1` will be available!
+
+To be able to use the serial port, the user has to be added to the `dialout` group:
+`usermod -a -G dialout`
+
+# AT Commands
+The device can respond to AT Commands.
+
+## Lamp sate
+```
+AT+LAMP=1
+AT+LAMP=0
+```
+
+## Neopixel color
+```
+AT+COLOR=0;0;0
+AT+COLOR=255;0;0
+AT+COLOR=0;255;0
+AT+COLOR=0;0;255
+```
+
+## Counter values
+```
+AT+COUNTER1=13
+AT+COUNTER2=9999
+```
---
+++ src/main.cpp
@@ -0,0 +1,149 @@
+#include <Arduino.h>
+#include <TM1637Display.h>
+#include <Adafruit_NeoPixel.h>
+
+#ifdef ESP8266
+#define WARNING_LIGHT_PIN D6
+// #define BUTTON_PIN D2
+#define NEOPIXEL_PIN D4
+#define DISPLAY_CLK D7
+#define DISPLAY_DIO D1
+#else
+#define DISPLAY_CLK 2
+#define DISPLAY_DIO 3
+#define NEOPIXEL_PIN 4
+#define WARNING_LIGHT_PIN 5
+#endif
+
+// void processConfigButton();
+void processDisplay();
+void displayColor(uint32_t color);
+
+TM1637Display display(DISPLAY_CLK, DISPLAY_DIO);
+Adafruit_NeoPixel pixels(1, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+uint8_t emptyLabel[] = {SEG_G, SEG_G, SEG_G, SEG_G};
+uint8_t topic1Label[] = {SEG_A | SEG_D | SEG_E | SEG_F | SEG_G, SEG_B | SEG_C | SEG_D | SEG_E | SEG_F, SEG_D | SEG_E | SEG_F | SEG_G, 0x00}; // eut
+uint8_t topic2Label[] = {
+    SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,
+    SEG_A | SEG_C | SEG_D | SEG_F | SEG_G,
+    SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G,
+    SEG_A | SEG_C | SEG_D | SEG_F | SEG_G,
+    SEG_A | SEG_B | SEG_C | SEG_E | SEG_F | SEG_G,
+}; // esas
+
+long beginDisplaySequenceMs = 0;
+long startWarningLight = false;
+int topic1CounterValue = 0;
+int topic2CounterValue = 0;
+
+void setup()
+{
+  pinMode(WARNING_LIGHT_PIN, OUTPUT);
+  // pinMode(BUTTON_PIN, INPUT_PULLUP);
+  digitalWrite(WARNING_LIGHT_PIN, HIGH);
+
+  Serial.begin(115200);
+  while (!Serial)
+    ; // wait for serial attach
+
+  beginDisplaySequenceMs = millis();
+
+  pixels.begin();
+  display.setBrightness(0x0f);
+
+  displayColor(pixels.Color(255, 0, 0));
+  display.setSegments(emptyLabel);
+}
+
+void loop()
+{
+  String value = Serial.readStringUntil('\n');
+  if (value.length() == 0)
+  {
+    return;
+  }
+
+  Serial.println(value);
+
+  if (value.startsWith("AT+COLOR="))
+  {
+    int r, g, b;
+    sscanf(value.c_str(), "AT+COLOR=%d;%d;%d", &r, &g, &b);
+    displayColor(pixels.Color(r, g, b));
+  }
+  else if (value.startsWith("AT+LAMP=ON"))
+  {
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(WARNING_LIGHT_PIN, LOW);
+  }
+  else if (value.startsWith("AT+LAMP=OFF"))
+  {
+    // turn the LED off by making the voltage LOW
+    digitalWrite(WARNING_LIGHT_PIN, HIGH);
+  }
+  else if (value.startsWith("AT+COUNTER1="))
+  {
+    sscanf(value.c_str(), "AT+COUNTER1=%d", &topic1CounterValue);
+  }
+  else if (value.startsWith("AT+COUNTER2="))
+  {
+    sscanf(value.c_str(), "AT+COUNTER2=%d", &topic2CounterValue);
+  }
+  else
+  {
+    Serial.println("ERROR");
+  }
+
+  // processConfigButton();
+
+  processDisplay();
+}
+
+// void processConfigButton()
+// {
+//   if (digitalRead(BUTTON_PIN) == LOW)
+//   {
+//     delay(1000);
+//     if (digitalRead(BUTTON_PIN) == LOW)
+//     {
+//       Serial.println("TODO");
+//     }
+//   }
+// }
+
+void processDisplay()
+{
+  if (topic1CounterValue == 0 && topic2CounterValue == 0)
+  {
+    display.setSegments(emptyLabel);
+    return;
+  }
+
+  long msSinceLastBeginSequence = millis() - beginDisplaySequenceMs;
+  if (msSinceLastBeginSequence < 2000)
+  {
+    display.setSegments(topic1Label);
+  }
+  else if (msSinceLastBeginSequence < 4000)
+  {
+    display.showNumberDec(topic1CounterValue, false);
+  }
+  else if (msSinceLastBeginSequence < 6000)
+  {
+    display.setSegments(topic2Label);
+  }
+  else if (msSinceLastBeginSequence < 8000)
+  {
+    display.showNumberDec(topic2CounterValue, false);
+  }
+  else
+  {
+    beginDisplaySequenceMs = millis();
+  }
+}
+
+void displayColor(uint32_t color)
+{
+  pixels.setPixelColor(0, color);
+  pixels.show();
+}
